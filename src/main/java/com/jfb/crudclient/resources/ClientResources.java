78b44d252e7441eba1b0b7b163d94dab3af4383e
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-10-06 15:25:25 -0300

    Client insert implemented.(ok)

--- src/main/java/com/jfb/crudclient/resources/ClientResources.java
+++ src/main/java/com/jfb/crudclient/resources/ClientResources.java
@@ -1,5 +1,7 @@
 package com.jfb.crudclient.resources;
 
+import java.net.URI;
+
 import com.jfb.crudclient.dto.ClientDTO;
 import com.jfb.crudclient.services.ClientService;
 
@@ -10,9 +12,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -37,6 +42,14 @@ public class ClientResources {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
